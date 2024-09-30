0ae615fc665a3f982d895ce906076431c7e89785
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2024-09-30 11:18:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let n1 = Number(prompt("Escreva a primeira nota"))
+let n2 = Number(prompt("Escreva a segunda nota"))
+let n3 = Number(prompt("Escreva a terceira nota"))
+let mediaFinal = (n1*2+n2*3+n3*5)/10
+
+alert("A média final é "+mediaFinal)
